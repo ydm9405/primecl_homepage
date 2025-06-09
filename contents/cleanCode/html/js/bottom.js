bcfda2f90f5e5d77643d972d1cf7c696dc11637e
ydm9405 <101791081+ydm9405@users.noreply.github.com>
2025-06-09 15:38:15 +0900

    Delete contents directory

--- contents/cleanCode/html/js/bottom.js
+++
@@ -1,9 +0,0 @@
-function setBottom() {
-  console.log("setBottom 호출");
-  $("#fs-footer").append(`
-      <div id="footer">
-          <div id="pageFooter">
-          </div>
-      </div>
-  `);
-}
--- contents/cleanCode/html/js/index.js
+++
@@ -1,8 +0,0 @@
-function setIndex() {
-  console.log("setIndex 호출");
-  $("#fs-index").append(`
-      <div id="index">
-        <div id="pageIndex"></div>
-      </div>
-  `);
-}
